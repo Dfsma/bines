cd93b3d6fe7775810ff2b23bbd3f691f339d5b32
Dfsma <diegof.df58@gmail.com>
2022-12-31 17:51:57 -0300

    dev: construcción del mantenedor de usuarios - servicio de usuarios

---
+++ mantenedor-usuarios/user-service/src/test/java/com/dfsma/bines/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dfsma.bines.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
