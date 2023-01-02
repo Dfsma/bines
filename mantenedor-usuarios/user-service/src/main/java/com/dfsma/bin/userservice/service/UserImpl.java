058d3956421d60b8751cfc0d639cf0bd971aa471
Dfsma <diegof.df58@gmail.com>
2023-01-02 20:22:55 -0300

    dev: Se agrega encriptacion y decriptacion de la contraseña

--- mantenedor-usuarios/user-service/src/main/java/com/dfsma/bin/userservice/service/UserImpl.java
+++ mantenedor-usuarios/user-service/src/main/java/com/dfsma/bin/userservice/service/UserImpl.java
@@ -4,6 +4,7 @@ import com.dfsma.bin.userservice.entity.UserEntity;
 import com.dfsma.bin.userservice.pojo.UserRequest;
 import com.dfsma.bin.userservice.pojo.response.ResponseMessage;
 import com.dfsma.bin.userservice.repository.UserRepository;
+import com.dfsma.bin.userservice.util.PasswordEncrypt;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,10 +19,10 @@ public class UserImpl implements UserService{
     private static final Logger logger = LoggerFactory.getLogger(UserImpl.class);
 
     private final UserRepository userRepository;
-
+    PasswordEncrypt passwordEncrypt = new PasswordEncrypt();
     ResponseMessage response = new ResponseMessage();
 
-    public UserImpl(UserRepository userRepository) {
+    public UserImpl(UserRepository userRepository) throws Exception {
         this.userRepository = userRepository;
     }
 
@@ -30,27 +31,31 @@ public class UserImpl implements UserService{
 
         ResponseEntity<ResponseMessage> httpResponse;
 
-        logger.info("--> Inicio mapeado de request a entidad");
-        UserEntity userEntity = new UserEntity(
-                userRequest.getCdgUserName(),
-                userRequest.getDscApellido(),
-                userRequest.getDscEmail(),
-                userRequest.getDscImagen(),
-                userRequest.getDscNombre(),
-                userRequest.getFlgActivo(),
-                userRequest.getValPassword(),
-                userRequest.getIdAplicacion()
-        );
-        logger.info("--> Fin mapeado de request a entidad");
-
         try {
-            UserEntity existUser = userRepository.findDscEmail(userEntity.getDscEmail());
+            UserEntity existUser = userRepository.findDscEmail(userRequest.getDscEmail());
             if (existUser != null) {
                 logger.info("--> El usuario ya existe");
                 response.ResponseMessage(400, "El usuario ya existe.");
                 httpResponse = new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
             } else {
                 logger.info("--> Inicio de guardado de usuario");
+                logger.info("--> Inicio mapeado de request a entidad");
+                logger.info("--> Inicio de encriptación de contraseña");
+                String encryptPassword = passwordEncrypt.encrypt(userRequest.getValPassword());
+                logger.info("--> Fin de encriptación de contraseña");
+                logger.info("--> Contraseña encriptada: {}", encryptPassword);
+                UserEntity userEntity = new UserEntity(
+                        userRequest.getCdgUserName(),
+                        userRequest.getDscApellido(),
+                        userRequest.getDscEmail(),
+                        userRequest.getDscImagen(),
+                        userRequest.getDscNombre(),
+                        userRequest.getFlgActivo(),
+                        encryptPassword,
+                        userRequest.getIdAplicacion()
+                );
+                logger.info("--> Desencriptando contraseña: {}", passwordEncrypt.decrypt(encryptPassword)); //TODO: Eliminar esta linea, solo es para probar la desencriptación
+                logger.info("--> Fin mapeado de request a entidad");
                 userRepository.save(userEntity);
                 logger.info("--> Usuario creado correctamente");
                 response.ResponseMessage(200,"Usuario creado correctamente");
@@ -79,6 +84,9 @@ public class UserImpl implements UserService{
             } else {
                 logger.info("--> Inicio de actualización de usuario");
                 logger.info("--> Inicio mapeado de request a entidad");
+                logger.info("--> Inicio de encriptación de contraseña");
+                String encryptPassword = passwordEncrypt.encrypt(userRequest.getValPassword());
+                logger.info("--> Fin de encriptación de contraseña");
                 UserEntity userEntity = new UserEntity(
                         existUser.getFchRegistro(),
                         userRequest.getCdgUserName(),
@@ -87,7 +95,7 @@ public class UserImpl implements UserService{
                         userRequest.getDscImagen(),
                         userRequest.getDscNombre(),
                         userRequest.getFlgActivo(),
-                        userRequest.getValPassword(),
+                        encryptPassword,
                         userRequest.getIdAplicacion()
                 );
                 logger.info("--> Fin mapeado de request a entidad");
